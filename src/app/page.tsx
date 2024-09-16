a9f9e3386a17f348208e933671756dd8bf9b81e6
midhun-coddletech <midhun.o@coddletech.com>
2024-09-16 08:16:45 +0530

    Add Statistics Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import FeatureSection from "./components/FeatureSection/FeatureSection";
 import GallerySection from "./components/GallerySection/GallerySection";
 import HeroBanner from "./components/HeroBanner/HeroBanner";
+import StatisticsSection from "./components/StatisticsSection/StatisticsSection";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <HeroBanner></HeroBanner>
       <FeatureSection></FeatureSection>
       <GallerySection></GallerySection>
+      <StatisticsSection></StatisticsSection>
   </div>
   );
 }
